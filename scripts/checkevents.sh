e393e1f26a874c30406ad5db4205b6d5e8dd5c7c
Fredrik Simonsson <fredrik@nodle.com>
2023-01-30 17:02:00 +0900

    Updated script

--- scripts/checkevents.sh
+++ scripts/checkevents.sh
@@ -3,6 +3,7 @@
 install -d ./target/eventcheck
 for i in many_secrets_discovery   simple_timed_lock  single_secret_discovery 
 do
+   echo Compare events for $i:
    cat ./target/ink/$i/$i.contract  | jq '{"V3"}[]."spec"."events"' > ./target/eventcheck/$i.json
-   diff ./golden/$i.json ./target/eventcheck/$i.json >/dev/null || echo '*** ERROR in '$i/events.json Definition changed
+   diff ./golden/$i.json ./target/eventcheck/$i.json ||  exit 1
 done
